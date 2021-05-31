fd9844a92911ed99fe140aad848a98f6ba096687
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-05-31 08:47:53 -0600

    mock data setup

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Csharp_REST_API.Models;
+
+namespace Csharp_REST_API.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command {Id = 0, HowTo = "Do something", Line = "something", Platform = "something else"},
+                new Command {Id = 1, HowTo = "Do something2", Line = "something2", Platform = "something else2"},
+                new Command {Id = 2, HowTo = "Do something3", Line = "something3", Platform = "something else3"}
+            };
+            
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id = 0, HowTo = "Do something", Line = "something", Platform = "something else"};
+        }
+    }
+}
