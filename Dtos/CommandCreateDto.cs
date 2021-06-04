f11c16569d870b7750efab813c5a82b13e2f42ea
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-06-04 14:38:28 -0600

    added annotations

--- Dtos/CommandCreateDto.cs
+++ Dtos/CommandCreateDto.cs
@@ -1,12 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Csharp_REST_API.Dtos
 {
     public class CommandCreateDto
     {
-        
+        [Required]
         public string HowTo { get; set; }
-
+        [Required]
         public string Line { get; set; }
-
+        [Required]
         public string Platform { get; set; }
     }
 }
