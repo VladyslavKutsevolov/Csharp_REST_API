ab6d10e45f38630e16b56a65dc0c7a82f9f96290
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-06-04 14:32:26 -0600

    create new command

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -31,7 +31,7 @@ namespace Csharp_REST_API.Controllers
             return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems)); 
         }
         
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetCommandById")]
         public ActionResult<CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
@@ -43,5 +43,20 @@ namespace Csharp_REST_API.Controllers
 
             return Ok(_mapper.Map<CommandReadDto>(commandItem));
         }
+
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommand(CommandCreateDto cmdCreateDto)
+        {
+            var commandModel = _mapper.Map<Command>(cmdCreateDto);
+            _repository.CreateCommand(commandModel);
+            _repository.SaveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+
+            return CreatedAtRoute(nameof(GetCommandById), new {Id = commandReadDto.Id}, commandReadDto);
+
+        }
+        
+            
     }
 }
